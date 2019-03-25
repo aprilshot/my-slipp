d90440d796f0eb94f42167e88341876eaae6494e
badugi <badugi30@gmail.com>
2019-03-26 02:40:48 +0900

    111

--- src/main/java/kr/co/kicom/MySlippApplication.java
+++ src/main/java/kr/co/kicom/MySlippApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MySlippApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(MySlippApplication.class, args);
 	}
 
